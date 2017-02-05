54e953f2393b704cfb83328746124916ea0df0c7
Schwalbe Sophie <sophie.schwalbe@digitalperformance.de>
2017-02-05 21:12:56 +0100

    view signature extended

--- src/View.php
+++ src/View.php
@@ -21,4 +21,17 @@ interface View
      * @return string
      */
     public function render();
+
+    /**
+     * @param string $controllerName
+     * @param string $actionName
+     * @return string
+     */
+    public function renderAction(string $controllerName, string $actionName);
+
+    /**
+     * @param string $viewPath path of the view file without any extension
+     * @return string
+     */
+    public function renderView(string $viewPath);
 }
